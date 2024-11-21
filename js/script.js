74b6e334d21fba8402529d82f49139261b55ec41
Linnéa Nilsson <linnea.nilsson97@hotmail.com>
2024-11-21 11:25:54 +0100

    sätta ihop alla html sidor

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// byte mellan flikarna
+const homeButton = document.querySelectorAll("#home-button");
+const gameButton = document.querySelectorAll("#start-button");
+const scoreButton = document.querySelectorAll("#score-button");
+
+const homePage = document.querySelector("#start-page");
+const gamePage = document.querySelector("#game-page");
+const scorePage = document.querySelector("#score-page");
+
+function hidePages() {
+  homePage.classList.add("hide-page");
+  gamePage.classList.add("hide-page");
+  scorePage.classList.add("hide-page");
+}
+
+homeButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    hidePages();
+    homePage.classList.remove("hide-page");
+  });
+});
+
+gameButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    hidePages();
+    gamePage.classList.remove("hide-page");
+  });
+});
+
+scoreButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    hidePages();
+    scorePage.classList.remove("hide-page");
+  });
+});
